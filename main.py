5c8b5ce0c495ca341816b000cac4e1652a9ccba4
CoyoteFox <gsfreeplay@gmail.com>
2021-11-03 16:17:09 -0400

    Changed times_run to times_looped

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import random 
 integer = 0
-times_run = 1
+times_looped = 1
 def numlist():
-  global times_run
+  global times_looped
   global integer
   while integer != 500:
     integer = random.randint(0,500)
     print(integer)
     if integer == 500:
       print()
-      print("This program looped",times_run,"times!")
+      print("This program looped",times_looped,"times!")
       integer = 500
     elif integer != 500:
-      times_run += 1
+      times_looped += 1
 numlist() 
