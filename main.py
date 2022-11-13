8d65c957fbd3b9211830bfbc57f1b854c5b31eae
CoyoteFox <gsfreeplay@gmail.com>
2022-11-12 21:47:19 -0500

    Added user interaction. Added try-except for goalNum Length.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import random 
 integer = 0
 times_looped = 1
-def numlist():
-  global times_looped
-  global integer
-  while integer != 500:
-    integer = random.randint(0,500)
-    print(integer)
-    if integer == 500:
-      print()
-      print("This program looped",times_looped,"times!")
-      integer = 500
-    elif integer != 500:
-      times_looped += 1
-numlist() 
+
+print()
+goalNum = int(input("What is your favorite large integer? "))
+print()
+
+try:
+  assert(len(str(goalNum)) >= 3), "ValueError: type int() has less than three (3) digits."
+except AssertionError as e:
+  print(e)
+  quit()
+
+  
+
+
+
+
+
+while integer != goalNum:
+  integer = random.randint(0,goalNum)
+  print(integer)
+  if integer == goalNum:
+    print()
+    if times_looped == goalNum:
+      print("This program ran exactly",goalNum,"times!")
+    elif times_looped == 1:
+      print("This program only ran once!")
+    else:
+      print("This program ran",times_looped,"times.")
+  times_looped += 1
+
